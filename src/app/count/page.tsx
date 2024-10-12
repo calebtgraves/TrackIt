f2bb34e436ece2bfedfbefa7c000303e3b626f5b
calebtgraves <calebtgraves@gmail.com>
2024-10-12 11:10:53 -0600

    'wip'

---
+++ src/app/count/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react';
+
+const CountPage: React.FC = () => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <h1>Count</h1>
+        </div>
+    );
+}
+
+export default CountPage;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useState } from 'react';
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    alert(`Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
