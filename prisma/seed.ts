a0bae41f4cfed3530bff7a9287798ddba8d5c6f0
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-10-15 16:03:50 -0600

    updated the seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,12 +5,47 @@ async function main() {
     where: { email: '1@1.com' },
     update: {},
     create: {
-      name: 'test',
+      id: '1',
+      name: 'Alice Active',
       email: 'test@test.com',
       password: 'password1',
       streaks: {
         create: [],
       },
+    },
+  });
+  await prisma.streaks.upsert({
+    where: { id: '1' },
+    update: {},
+    create: {
+      id: '1',
+      name: 'Streak 1',
+      goal: 'Goal 1',
+      type: 'check',
+      userId: '1',
+      totalCount: null,
+      unit: null,
+      totalQuantity: null,
+      totalTime: null,
+      reportType: null,
+      totalInputs: 0,
+    },
+  });
+  await prisma.streaks.upsert({
+    where: { id: '1' },
+    update: {},
+    create: {
+      id: '2',
+      name: 'Streak 2',
+      goal: 'Goal 2',
+      type: 'check',
+      userId: '2',
+      totalCount: null,
+      unit: null,
+      totalQuantity: null,
+      totalTime: null,
+      reportType: null,
+      totalInputs: 0,
     },
   });
 }
